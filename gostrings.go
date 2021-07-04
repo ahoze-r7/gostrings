6c0d38ab502dfa151a04626986c267cdaf52c080
Avidan Hoze <avidan@alcide.io>
2021-07-04 13:11:46 +0300

    change fike

--- gostrings.go
+++ gostrings.go
@@ -12,6 +12,7 @@ func ReverseRunes(s string) string {
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
+
 	gostrings2.ReverseRunes2("sd")
 
 	return string(r)
