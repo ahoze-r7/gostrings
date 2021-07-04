a87aef86c98ed20b06a8aba9a33d30df7963a80e
Avidan Hoze <avidan@alcide.io>
2021-07-04 12:23:32 +0300

    add go.mod

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ahoze-r7/gostrings
+
+go 1.15
+
+require (
+    github.com/ahoze-r7/gostrings2 v1.0.1
+)
--- gostrings.go
+++ gostrings.go
@@ -2,12 +2,17 @@
 // encoded strings, beyond what is provided in the standard "strings" package.
 package gostrings
 
+import (
+	"github.com/ahoze-r7/gostrings2"
+)
+
 // ReverseRunes returns its argument string reversed rune-wise left to right.
 func ReverseRunes(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
+	gostrings2.ReverseRunes2("sd")
 
 	return string(r)
 }
